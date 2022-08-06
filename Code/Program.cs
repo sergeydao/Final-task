715179e9f235362a719936e7512b568b89adfd07
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-08-06 16:16:01 +0300

    Added variation#1 code

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿// variation#1
+
+string[] array = { "hello", "2", "world", ":-)", "**", "000" };
+string[] finalArray = new string[6];
+string temporaryArray = String.Empty;
+int index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    temporaryArray = array[i];
+    int length = temporaryArray.Length;
+    int count = 0;
+    while (length > 0)
+    {
+        count++;
+        length--;
+    }
+    if (count <= 3)
+    {
+        finalArray[index] = temporaryArray;
+        index++;
+    }
+}
+
+PrintArray(finalArray);
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
